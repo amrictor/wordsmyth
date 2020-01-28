21f0171582b38b6d2a332cf397f6d9fc33115d15
Abby Rictor <amrictor@gmail.com>
2020-01-27 18:21:02 -0700

    Updates

---
+++ precache-manifest.1f4ea495eedf4e30c5f23b203c61af65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0836a7092d9abdd9cb606306ccee3159",
+    "url": "/static/media/loadingicon.0836a709.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e24946b0ab9fe27e98cf",
+    "url": "/static/js/main.e24946b0.chunk.js"
+  },
+  {
+    "revision": "78bf1a6e350615dbeec0",
+    "url": "/static/js/1.78bf1a6e.chunk.js"
+  },
+  {
+    "revision": "e24946b0ab9fe27e98cf",
+    "url": "/static/css/main.6a8705e4.chunk.css"
+  },
+  {
+    "revision": "78bf1a6e350615dbeec0",
+    "url": "/static/css/1.d39fa042.chunk.css"
+  },
+  {
+    "revision": "bda43b7ae6eb96882815f78d79bfba70",
+    "url": "/index.html"
+  }
+];
